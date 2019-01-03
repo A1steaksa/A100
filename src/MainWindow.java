a4a06e382c2b87a612092180d91488850a04f105
John Schneider <a1steaksa@users.noreply.github.com>
2019-01-03 15:35:23 -0600

    Register format is mostly sorted
    Now onto getting play, stop, step, and fast forward buttons working

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -1,4 +1,5 @@
 import java.awt.BorderLayout;
+import java.awt.Color;
 import java.awt.Dimension;
 import java.awt.Font;
 import java.awt.event.ActionEvent;
@@ -15,6 +16,7 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.Scanner;
 
+import javax.swing.BorderFactory;
 import javax.swing.BoxLayout;
 import javax.swing.DefaultListModel;
 import javax.swing.ImageIcon;
@@ -45,45 +47,48 @@ public class MainWindow extends JFrame{
 
 	//The program title
 	String titleBase = "A100";
-	
+
 	//The currently open file
 	String openFile = "";
-	
+
 	//The file extension to expect
 	String fileExtension = "A1";
-	
+
 	//The number of registers
-	int registerCount = 20;
-	
+	int registerCount = 8;
+
 	//The actual registers themselves
 	int[] registers = new int[ registerCount ];
-	
+
 	//Stores the labels associated with the registers
 	JLabel[] registerLabels = new JLabel[ registerCount ];
-	
+
 	//Path to the icons
 	String iconPath = "resources/icons/";
-	
+
 	//Keeps track of whether or not the file has been changed
 	boolean fileHasChanged = false;
+
+	//Keeps track of whether the code is currently executing
+	boolean isRunning = false;
 	
 	//The font size
 	int fontSize = 18;
-	
+
 	//The path of the most recently opened file
 	//Default to execution path
 	File currentFile = new File( System.getProperty("user.dir") );
-	
+
 	//UI Variables
 	//Buttons
 	JButton saveButton;
-	
+
 	//Text area
 	JTextArea codeTextArea;
-	
+
 	MainWindow() throws ClassNotFoundException, InstantiationException, IllegalAccessException, UnsupportedLookAndFeelException{
 		super();
-		
+
 		//Font
 		Charset.forName( "UTF-8" );
 		Font font = new Font( "Consolas", Font.PLAIN, fontSize );
@@ -98,11 +103,11 @@ public class MainWindow extends JFrame{
 		//Main window properties
 		this.setTitle( titleBase );
 		this.setSize( new Dimension( 500, 750 ) );
-		this.setMinimumSize( new Dimension( 200, 200 ) );
+		this.setMinimumSize( new Dimension( 500, 500 ) );
 		this.setLocationByPlatform( true );
 		this.setLayout( new BorderLayout() );
 		this.setDefaultCloseOperation( JFrame.DO_NOTHING_ON_CLOSE );
-		
+
 		//Confirm close if there are unsaved changes 
 		this.addWindowListener( new WindowListener() {
 
@@ -114,33 +119,33 @@ public class MainWindow extends JFrame{
 
 			@Override
 			public void windowClosing(WindowEvent arg0) {
-				
+
 				//Ask them if they want to close without saving
 				if( fileHasChanged ) {
-					
+
 					int dialogResult = JOptionPane.showConfirmDialog(
-						codeTextArea,
-						"There are unsaved changes to this file.\n\nWould you like to save before exiting?",
-						"Warning",
-						JOptionPane.YES_NO_CANCEL_OPTION
-					);
-					
+							codeTextArea,
+							"There are unsaved changes to this file.\n\nWould you like to save before exiting?",
+							"Warning",
+							JOptionPane.YES_NO_CANCEL_OPTION
+							);
+
 					//If they do want to save
 					if( dialogResult == JOptionPane.YES_OPTION ){
-						
+
 						//Let them save
 						doASave();
 					}
-					
+
 					//If they click cancel, pretend they didn't try to exit
 					if( dialogResult == JOptionPane.CANCEL_OPTION ){
 						return;
 					}
 				}
-				
+
 				//Having dealt with that, exit the program
 				System.exit( 1 );
-				
+
 			}
 
 			@Override
@@ -154,20 +159,20 @@ public class MainWindow extends JFrame{
 
 			@Override
 			public void windowOpened(WindowEvent arg0) {}
-			
+
 		});
-		
+
 		//Text pane
 		codeTextArea = new JTextArea();
 		codeTextArea.setFont( font );
 		codeTextArea.setTabSize( 3 );
+		codeTextArea.setDocument( new UpperCaseDocument() );
 		
 		//Listener to catch changes
 		codeTextArea.getDocument().addDocumentListener( new DocumentListener() {
-			
+
 			@Override
-			public void changedUpdate(DocumentEvent e) {
-			}
+			public void changedUpdate(DocumentEvent e) {}
 
 			@Override
 			public void insertUpdate(DocumentEvent e) {
@@ -182,9 +187,9 @@ public class MainWindow extends JFrame{
 					setFileHasChanged( true );
 				}
 			}
-			
+
 		});
-		
+
 		//Keyboard listener for ctrl-s
 		codeTextArea.addKeyListener( new KeyListener() {
 
@@ -205,11 +210,10 @@ public class MainWindow extends JFrame{
 					if( controlDown ) {
 						doASave();
 
-						//And avoid doing this again by accident
-						controlDown = false;
+						event.consume();
 					}
 				}
-
+				
 			}
 
 			@Override
@@ -229,7 +233,7 @@ public class MainWindow extends JFrame{
 			}
 
 		});
-		
+
 		//Code line numbers
 		TextLineNumber codeLineNumber = new TextLineNumber( codeTextArea );
 		codeLineNumber.setFont( font );
@@ -237,51 +241,68 @@ public class MainWindow extends JFrame{
 		//Code scroll pane
 		JScrollPane codeScrollPane = new JScrollPane( codeTextArea );
 		codeScrollPane.setRowHeaderView( codeLineNumber );
-		
+
 		//Registers
 		JPanel registersPanel = new JPanel();
 		registersPanel.setLayout( new BoxLayout( registersPanel, BoxLayout.X_AXIS ) );
-		registersPanel.setPreferredSize( new Dimension( -1, 30 ) );
+		registersPanel.setPreferredSize( new Dimension( 50 * registerCount, 50 ) );
 		
-        DefaultListModel<String> listModel = new DefaultListModel<String>();
-        
-		//Add register labels
 		for (int i = 0; i < registerCount; i++) {
 			
-			listModel.addElement( "R" + i );
+			//The panel to contain the register
+			JPanel register = new JPanel();
+			register.setLayout( new BorderLayout() );
+			register.setMinimumSize( new Dimension( 50, -1 ) );
+			register.setPreferredSize( new Dimension( 50, -1 ) );
+			
+			//Add a border
+			register.setBorder(BorderFactory.createLineBorder(Color.black));
+			
+			//The label to hold the register name
+			JLabel topLabel = new JLabel( "R" + i );
+			topLabel.setHorizontalAlignment( JLabel.CENTER );
+			topLabel.setFont( font );
+			register.add( topLabel, BorderLayout.NORTH );
+			
+			//The label to hold the register value
+			JLabel bottomLabel = new JLabel( "0" );
+			bottomLabel.setHorizontalAlignment( JLabel.CENTER );
+			bottomLabel.setFont( font );
+			register.add( bottomLabel, BorderLayout.SOUTH );
+			
+			//Add the bottom register to the registerLabels list so it can be edited later
+			registerLabels[ i ] = bottomLabel;
+			
+			//Add this register to the list
+			registersPanel.add( register );
 		}
 		
-		JList<String> list = new JList<String>( listModel );
-        list.setVisibleRowCount( 1 );
-        list.setLayoutOrientation( JList.HORIZONTAL_WRAP );
-        
-        registersPanel.add( list );
-		
+
 		//Registers scroll pane
 		JScrollPane registersPanelScrollPane = new JScrollPane( registersPanel );
-		
+
 		//Console
 		JTextArea consoleTextArea = new JTextArea( 3, 10 );
 		consoleTextArea.setFont( font );
 		consoleTextArea.setTabSize( 3 );
 		consoleTextArea.setEditable( false );
-		
+
 		//Console scroll pane
 		JScrollPane consoleScrollPane = new JScrollPane( consoleTextArea );
-		
+
 		//Panel for the console and registers
 		JPanel bottomPanel = new JPanel();
 		bottomPanel.setLayout( new BorderLayout() );
 		bottomPanel.add( registersPanelScrollPane, BorderLayout.NORTH );
 		bottomPanel.add( consoleScrollPane, BorderLayout.CENTER );
-		
+
 		//Split pane to split code from the bottom pieces
 		JSplitPane topSplitPane = new JSplitPane( JSplitPane.VERTICAL_SPLIT );
 		topSplitPane.setTopComponent( codeScrollPane );
 		topSplitPane.setBottomComponent( bottomPanel );
 		topSplitPane.setDividerLocation( 500 );
 		this.add( topSplitPane, BorderLayout.CENTER );
-		
+
 		//Tool bar
 		JToolBar toolbar = new JToolBar();
 		toolbar.setFloatable( false );
@@ -299,17 +320,17 @@ public class MainWindow extends JFrame{
 				//Make sure they want to lose everything if there is something to lose
 				if( fileHasChanged ) {
 					int dialogResult = JOptionPane.showConfirmDialog(
-						codeTextArea,
-						"Creating a new file will destroy any changes in the current file.\n\nAre you sure you want to lose everything?",
-						"Warning",
-						JOptionPane.YES_NO_OPTION
-					);
-					
+							codeTextArea,
+							"Creating a new file will destroy any changes in the current file.\n\nAre you sure you want to lose everything?",
+							"Warning",
+							JOptionPane.YES_NO_OPTION
+							);
+
 					//If they do
 					if(dialogResult == JOptionPane.NO_OPTION){
 						return;
 					}
-					
+
 					//Close the current file
 					closeFile();
 				}
@@ -327,44 +348,44 @@ public class MainWindow extends JFrame{
 
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				
+
 				//Have them select a new file
 				JFileChooser fileChooser = new JFileChooser();
 				fileChooser.setFileFilter( new FileNameExtensionFilter( "A100 Files", fileExtension )  );
 				fileChooser.setCurrentDirectory( currentFile ); 
-				
+
 				//Get their response
 				int returnVal = fileChooser.showOpenDialog( openButton );
-				
+
 				//Open a file if they chose one
 				if( returnVal == JFileChooser.APPROVE_OPTION ) {
-					
+
 					//Make sure they want to lose everything if there is something to lose
 					if( fileHasChanged ) {
-						
+
 						//Make sure they want to lose everything
 						int dialogResult = JOptionPane.showConfirmDialog(
-							codeTextArea,
-							"Opening a file will destroy any changes in the current file.\n\nAre you sure you want to lose everything?",
-							"Warning",
-							JOptionPane.YES_NO_OPTION
-						);
-						
+								codeTextArea,
+								"Opening a file will destroy any changes in the current file.\n\nAre you sure you want to lose everything?",
+								"Warning",
+								JOptionPane.YES_NO_OPTION
+								);
+
 						//If they do
 						if(dialogResult == JOptionPane.NO_OPTION){
 							return;
 						}
-						
+
 					}
-					
+
 					//Close the current file
 					closeFile();
-					
+
 					//Open the new file
 					File file = fileChooser.getSelectedFile();
 					openFile( file );
 				}
-				
+
 			}
 
 		});
@@ -384,8 +405,8 @@ public class MainWindow extends JFrame{
 
 		});
 		toolbar.add( saveButton );
+
 		
-		/*
 		toolbar.addSeparator();
 
 		//Run button
@@ -407,23 +428,26 @@ public class MainWindow extends JFrame{
 		ImageIcon stepIcon = new ImageIcon( iconPath + "step.png" );
 		JButton stepButton = new JButton( stepIcon );
 		stepButton.setFocusable( false );
+		stepButton.setEnabled( false );
 		stepButton.setToolTipText( "Step" );
 		stepButton.addActionListener( new ActionListener() {
 
 			@Override
 			public void actionPerformed(ActionEvent arg0) {
-				System.out.println( "Step" );
+				
+				
+				
 			}
 
 		});
 		toolbar.add( stepButton );
-		
-		*/
+
+		 
 
 		this.add( toolbar, BorderLayout.NORTH );
 
 		this.setVisible( true );
-		
+
 		//Make sure we properly set up the empty default file
 		closeFile();
 
@@ -433,144 +457,144 @@ public class MainWindow extends JFrame{
 	public boolean isFileOpen() {
 		return !openFile.equals( "" );
 	}
-	
+
 	//Figures out what kind of save to do and does it
 	public void doASave() {
-		
+
 		//If the file has changed, show the save dialog
 		//If there is already a file open, just save to that
 		if( isFileOpen() ) {
 			saveFile( currentFile );
 		}else {
-			
+
 			//Otherwise, show the dialog
 			showSaveFileDialog();
 		}
-		
+
 	}
-	
+
 	//Shows the save file dialog
 	public void showSaveFileDialog() {
-		
+
 		JFileChooser fileChooser = new JFileChooser();
 		fileChooser.setFileFilter( new FileNameExtensionFilter( "A100 Files", fileExtension )  );
 		fileChooser.setCurrentDirectory( currentFile ); 
-		
+
 		//Get their response
 		int returnVal = fileChooser.showSaveDialog( codeTextArea );
-		
+
 		//Open a file if they chose one
 		if( returnVal == JFileChooser.APPROVE_OPTION ) {
-			
+
 			File saveFile = fileChooser.getSelectedFile();
-			
+
 			//If it doesn't end in the appropriate extension, add it on
 			if( !saveFile.getName().toLowerCase().endsWith( "." + fileExtension.toLowerCase() ) ) {
-				
+
 				saveFile = new File( saveFile.getAbsolutePath() + "." + fileExtension );
-				
+
 			}
-			
+
 			saveFile( saveFile );
-			
+
 		}
-		
+
 	}
-	
+
 	//Actually saves the currently open file
 	public void saveFile( File file ) {
-		
+
 		//Writer to save the file
 		try {
 			FileWriter writer = new FileWriter( file );
-			
+
 			//Save the file
 			writer.write( codeTextArea.getText() );
-			
+
 			//Close the writer
 			writer.flush();
 			writer.close();
-			
+
 			//If we just saved, make sure we know we don't have changes left
 			setFileHasChanged( false );
-			
+
 			//Update our recent file path
 			currentFile = file;
-			
+
 			//Keep track of our file name
 			openFile = file.getName();
-			
+
 			//Update the title
 			updateTitle();
-			
+
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 	}
-	
+
 	//Opens a file
 	public void openFile( File file ) {
-		
+
 		//Update the open file
 		openFile = file.getName();
-		
+
 		//Mark down that this is the path to the most recent file
 		currentFile = file;
-		
+
 		//The contents of the file
 		String fileContents = "";
-		
+
 		try {
-			
+
 			//Scanner to read the file
 			Scanner scanner = new Scanner( new FileReader( file ) );
-			
+
 			//Read the contents
 			while( scanner.hasNext() ) {
-				
+
 				String line = scanner.nextLine();
-				
+
 				fileContents += line + System.lineSeparator();
-				
+
 			}
-			
+
 			//Close the scanner
 			scanner.close();
-			
+
 			//Put the loaded file in the text area
 			codeTextArea.setText( fileContents );
-			
+
 			//Update the title
 			updateTitle();
-			
+
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		
+
 		//Just opened files have no changes
 		setFileHasChanged( false );
-		
+
 	}
-	
+
 	//Closes the current file
 	public void closeFile() {
-		
+
 		//We have no file open
 		openFile = "";
-		
+
 		//Update the title to reflect that
 		updateTitle();
-		
+
 		//Empty any text that might linger
 		codeTextArea.setText( "" );
-		
+
 		//Empty files have no changes
 		setFileHasChanged( false );
 	}
-	
+
 	//Updates the title of the window
 	public void updateTitle() {
 		if( isFileOpen() ) {
@@ -579,12 +603,12 @@ public class MainWindow extends JFrame{
 			this.setTitle( titleBase + " - Untitled" );
 		}
 	}
-	
+
 	//Updates the file having changed
 	public void setFileHasChanged( boolean newValue ) {
-		
+
 		fileHasChanged = newValue;
-		
+
 	}
 
 }
