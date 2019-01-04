4c1e3077d9c2fd10ed5f55f33b068c68c85b0f02
John Schneider <a1steaksa@users.noreply.github.com>
2019-01-03 20:37:40 -0600

    MOV works!
    It ran it's first program! I'm so proud of it

--- src/MainWindow.java
+++ src/MainWindow.java
@@ -528,7 +528,7 @@ public class MainWindow extends JFrame{
 	public void error( String str ) {
 		
 		//Print the error
-		print( str );
+		print( "Line #" + ( programCounter + 1 ) + ": " + str );
 		
 		//Halt
 		switchToEditMode();
@@ -578,6 +578,12 @@ public class MainWindow extends JFrame{
 
 		//Clear the console
 		clearConsole();
+		
+		//Clear the registers
+		clearRegisters();
+		
+		//Reset the program counter
+		programCounter = 0;
 
 		//Disable editing items
 		saveButton.setEnabled( false );
@@ -776,16 +782,61 @@ public class MainWindow extends JFrame{
 		if( !argument.startsWith( "R" ) ){
 			return false;
 		}
-			
+		
+		//Get rid of the R so we can convert to a number
 		argument = argument.replace( "R", "" );
 		
-		//But also if the register it references is outside the register count that's not good
-		if(  ) {
-			
+		int registerNumber;
+		
+		//Convert to a register number
+		try {
+			registerNumber = Integer.parseInt( argument );
+		} catch( Exception e ) {
+			//If we can't convert the remaining text to a number, this was not a register
+			return false;
 		}
+		
+		//But also if the register it references is outside the register count that's not good
+		if( registerNumber >= registerCount || registerNumber < 0 ) {
+			return false;
+		}
+		
+		//Only now can we say it's probably a register (probably)
+		return true;
 	}
 	
+	//Parses the register number out of a register string
+	public int getRegisterNumber( String argument ) {
+		
+		//Remove the R
+		argument = argument.replace( "R" ,  "" );
+		
+		//Trim the result
+		argument = argument.trim();
+		
+		//Convert to an int
+		int output = Integer.parseInt( argument );
+		
+		return output;
+	}
 	
+	//Stores value A in register B
+	public void storeValueInRegister( int A, int B ) {
+		
+		//Store the value
+		registers[ B ] = A;
+		
+		//Update the UI
+		registerLabels[ B ].setText( String.valueOf( A  ) );
+		
+	}
+	
+	//Clears the registers
+	public void clearRegisters() {
+		for (int i = 0; i < registerCount; i++) {
+			storeValueInRegister( 0, i );
+		}
+	}
 	
 	//Performs the MOV command
 	public void MOV( String A, String B ) {
@@ -796,7 +847,27 @@ public class MainWindow extends JFrame{
 		//If A is not a register, we need to get the value from what A references
 		if( !isRegister( A ) ) {
 			
+			//Convert A to an integer
+			try {
+				AValue = Integer.parseInt( A );
+			} catch ( Exception e ) {
+				//If this isn't a register, and it isn't a literal number, we don't know what it is then
+				error( Strings.UnrecognizedDataType );
+			}
+			
+			//Now AInt contains the literal value of A
+		}else {
+			//If A is a register, we need to go get it's value
+			
+			//Get the register number
+			int ARegisterNumber = getRegisterNumber( A );
+			
+			//Get A's register's value
+			AValue = registers[ ARegisterNumber ];
 		}
+		
+		//Get B's register number
+		BValue = getRegisterNumber( B );
 		
 		//Error checking
 		
@@ -806,12 +877,12 @@ public class MainWindow extends JFrame{
 		}
 		
 		//Check the numbers for out of bounds error
-		if( AValue > 999 || AValue < -999 || BValue > 999 || BValue < -999 ) {
+		if( AValue > 999 || AValue < -999 ) {
 			error( Strings.NumberOutOfBounds );
 		}
 		
 		//If there are no errors, store AValue in the register corresponding to BValue
-		
+		storeValueInRegister( AValue, BValue );
 		
 	}
 
@@ -850,20 +921,21 @@ public class MainWindow extends JFrame{
 				//Break the line apart by spaces as those are our delimiter
 				String[] splitLine = line.split( " " );
 				
-				//Check for MOV
-				if( splitLine[ 0 ].equals( "MOV" ) ) {
-					
+				//Trim everything to avoid errors later
+				for (int i = 0; i < splitLine.length; i++) {
+					splitLine[ i ] = splitLine[ i ].trim();
+				}
+				
+				//We're going to use a switch case for this
+				switch( splitLine[ 0 ] ) {
+				case "MOV":
 					MOV( splitLine[ 1 ], splitLine[ 2 ] );
+					break;
 					
 				}
 				
 			}
 			
-			
-			
-			
-
-
 			programCounter++;
 		}else {
 			//Stop executing if we have run out of stuff to execute
--- src/Strings.java
+++ src/Strings.java
@@ -3,6 +3,10 @@ public class Strings {
 	
 	public static String NumberOutOfBounds = "A number was stored that is outside the bounds [-999, 999]!";
 	
-	public static String ArgumentIsNotRegister = "An argument was expected to be a register but was not!";
+	public static String ArgumentIsNotRegister = "An argument was expected to be a register but was not or was out of bounds!";
+	
+	public static String InvalidRegisterReference = "A reference to a non-existant register was made!";
+	
+	public static String UnrecognizedDataType = "An argument was neither a register reference or a literal number!";
 	
 }
